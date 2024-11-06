73c675ef69033f55edb25650dbab1f786d1812af
Fhanafii <fhanafi544@gmail.com>
2024-11-06 09:42:51 +0700

    Adding getDetailStory to pass into DetailStoryViewModel

--- app/src/main/java/com/fhanafi/storyapp/data/StoryRepository.kt
+++ app/src/main/java/com/fhanafi/storyapp/data/StoryRepository.kt
@@ -1,5 +1,6 @@
 package com.fhanafi.storyapp.data
 
+import com.fhanafi.storyapp.data.remote.response.DetailStoryResponse
 import com.fhanafi.storyapp.data.remote.response.StoryResponse
 import com.fhanafi.storyapp.data.remote.retrofit.ApiConfig
 import com.fhanafi.storyapp.data.remote.retrofit.ApiService
@@ -11,6 +12,11 @@ class StoryRepository private constructor(
     suspend fun getStories(token: String): StoryResponse {
         val apiService = ApiConfig().getApiService(token)
         return apiService.getStories()
+    }
+
+    suspend fun getDetailStory(token: String, storyId: String): DetailStoryResponse {
+        val apiService = ApiConfig().getApiService(token)
+        return apiService.getDetailStory(storyId)
     }
 
     companion object {
