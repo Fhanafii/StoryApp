e2514dc250240d9f07c677a3f57709778a3f908e
Fhanafii <fhanafi544@gmail.com>
2024-11-06 23:08:58 +0700

    Adding uploadStory to Repository

--- app/src/main/java/com/fhanafi/storyapp/data/StoryRepository.kt
+++ app/src/main/java/com/fhanafi/storyapp/data/StoryRepository.kt
@@ -1,9 +1,12 @@
 package com.fhanafi.storyapp.data
 
 import com.fhanafi.storyapp.data.remote.response.DetailStoryResponse
+import com.fhanafi.storyapp.data.remote.response.FileUploadResponse
 import com.fhanafi.storyapp.data.remote.response.StoryResponse
 import com.fhanafi.storyapp.data.remote.retrofit.ApiConfig
 import com.fhanafi.storyapp.data.remote.retrofit.ApiService
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 class StoryRepository private constructor(
     private val apiService: ApiService
@@ -17,6 +20,11 @@ class StoryRepository private constructor(
     suspend fun getDetailStory(token: String, storyId: String): DetailStoryResponse {
         val apiService = ApiConfig().getApiService(token)
         return apiService.getDetailStory(storyId)
+    }
+
+    suspend fun uploadStory(token: String, imageFile: MultipartBody.Part, description: RequestBody): FileUploadResponse {
+        val apiService = ApiConfig().getApiService(token)
+        return apiService.uploadStory(imageFile, description)
     }
 
     companion object {
