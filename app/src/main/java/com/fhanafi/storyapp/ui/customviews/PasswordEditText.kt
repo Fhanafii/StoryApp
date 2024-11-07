b8326bd14180971182c4d02b224c65cbdda9507f
Fhanafii <fhanafi544@gmail.com>
2024-11-07 13:45:21 +0700

    Adding custom view for highlight password less then 8 char

---
+++ app/src/main/java/com/fhanafi/storyapp/ui/customviews/PasswordEditText.kt
@@ -0,0 +1,26 @@
+package com.fhanafi.storyapp.ui.customviews
+
+import android.content.Context
+import android.text.InputType
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.widget.doOnTextChanged
+
+class PasswordEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = android.R.attr.editTextStyle
+) : AppCompatEditText(context, attrs, defStyleAttr) {
+
+    init {
+        inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        isFocusable = true
+        isClickable = true
+        isFocusableInTouchMode = true
+
+        // Optional: Add error message if password is less than 8 characters
+        doOnTextChanged { text, _, _, _ ->
+            if (text != null && text.length < 8) {
+                error = "Password must be at least 8 characters"
+            }
+        }
+    }
+}
